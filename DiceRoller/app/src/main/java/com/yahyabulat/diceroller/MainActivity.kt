184f798627088ce9545bab26d8094e824653860c
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-09-30 13:04:45 +0300

    Dice Roller project done

--- DiceRoller/app/src/main/java/com/yahyabulat/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/yahyabulat/diceroller/MainActivity.kt
@@ -19,7 +19,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview

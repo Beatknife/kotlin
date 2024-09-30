1ab51e6ed291dac1a695d921796227e9aa2e3fbc
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-09-30 13:06:03 +0300

    GreetingCard project done

--- GreetingCard/app/src/main/java/com/yahyabulat/greetingcard/MainActivity.kt
+++ GreetingCard/app/src/main/java/com/yahyabulat/greetingcard/MainActivity.kt
@@ -19,7 +19,6 @@ import com.yahyabulat.greetingcard.ui.theme.GreetingCardTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             GreetingCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
